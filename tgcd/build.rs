0d789d5c5405db19418508db182721bc976c64ce
foldu <foldu@protonmail.com>
2020-02-16 15:09:05 +0100

    Always use the system protoc

--- tgcd/build.rs
+++ tgcd/build.rs
@@ -1,3 +1,6 @@
-fn main() {
-    tonic_build::compile_protos("proto/tgcd.proto").unwrap();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let protoc = which::which("protoc").map_err(|e| format!("`protoc` not found, please install it: {}", e))?;
+    std::env::set_var("PROTOC", &protoc);
+    tonic_build::compile_protos("proto/tgcd.proto")?;
+    Ok(())
 }
